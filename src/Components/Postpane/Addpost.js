3cd038c55b3bf3e7cd49532bef74ee70106fef8b
hasithasd4 <hasithasd4@gmail.com>
2023-10-02 21:05:54 +0530

    edit post

--- src/Components/Postpane/Addpost.js
+++ src/Components/Postpane/Addpost.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import './AddPost.css'
 import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
+import AddLocationAltIcon from '@mui/icons-material/AddLocationAlt';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
 
 export default function Addpost() {
   return (
@@ -15,8 +18,28 @@ export default function Addpost() {
         <hr className='posthr'></hr>
         <div className="addpostbuttom">
              <div className="PostAction">
+              <div className="addphotooptions">
+                <div className="addphotooption">
                 <AddAPhotoIcon className='addpoto'/>
                 <span className="addpostOptiontxt">Add Photo/Video</span>
+                </div>
+
+                <div className="addphotooption">
+                <AddLocationAltIcon className='addpoto'/>
+                <span className="addpostOptiontxt">Add Lcation</span>
+                </div>
+
+                <div className="addphotooption">
+                <FaceRetouchingNaturalIcon className='addpoto'/>
+                <span className="addpostOptiontxt">Tag</span>
+                </div>
+
+                <div className="addphotooption">
+                <LiveTvIcon className='addpoto'/>
+                <span className="addpostOptiontxt">Go Live</span>
+                </div>
+              </div>
+               
              </div>
             
         </div>
